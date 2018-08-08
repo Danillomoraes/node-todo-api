a84264c46a43ed8f9834bbd62a1af1e69a6f1441
danillo.moraes <danillo.moraes@outlook.com>
2018-08-08 17:22:18 -0300

    initial commit

---
+++ playground/mongodb-connet.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('unable to connet to mongodb, error: '+err);
+  }
+    console.log('Connect to Mongo server');
+
+  db.collection('Todos').insertOne({
+    text: 'lavar roupa',
+    completed: false
+  }, (err, result) => {
+    if(err) {
+      return console.log('Unable to insert todo', err);
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.close();
+
+});
