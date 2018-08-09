a4254db51307b84108180a3cd1b3c9eab2e8ca7b
danillo.moraes <danillo.moraes@outlook.com>
2018-08-09 10:38:19 -0300

    add connection with mongo

--- playground/mongodb-connet.js
+++ playground/mongodb-connet.js
@@ -1,4 +1,9 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = ObjectID();
+// console.log(obj);
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if(err) {
@@ -6,9 +11,11 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
     console.log('Connect to Mongo server');
 
+
+
   db.collection('Todos').insertOne({
     text: 'lavar roupa',
-    completed: false
+    completed: true
   }, (err, result) => {
     if(err) {
       return console.log('Unable to insert todo', err);
@@ -20,3 +27,25 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   db.close();
 
 });
+
+
+// MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+//   if (err) {
+//     return console.log('unable to connect to mongo');
+//   }
+//
+//   db.collection('Users').insertOne({
+//     name: 'vagnerca',
+//     age: 37,
+//     localtion:'São paulo, SP'
+//   }, (err, result) => {
+//     if (err) {
+//       return console.log('Unable to insert user', err);
+//     }
+//
+//     console.log(JSON.stringify(result.ops, undefined, 2));
+//   });
+//
+//   db.close();
+//
+// });
---
+++ playground/mongodb-find.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = ObjectID();
+// console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('unable to connet to mongodb, error: '+err);
+  }
+    console.log('Connect to Mongo server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5b6c41781cf1a6228005fa9a')
+  // }).toArray().then((docs) =>{
+  //
+  //   console.log(docs);
+  //
+  // }, (err) => {
+  //   if (err) {
+  //     console.log('unable to find document', err);
+  //   }
+  // });
+
+  db.collection('Todos').find().count().then((count) =>{
+
+    console.log(`Todos count: ${count}`);
+
+  }, (err) => {
+    if (err) {
+      console.log('unable to find document', err);
+    }
+  });
+
+
+  db.close();
+
+});
