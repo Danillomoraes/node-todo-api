210068079b0f21f8c631a644f0302fd798d50846
danillo.moraes <danillo.moraes@outlook.com>
2018-08-10 17:49:33 -0300

    tests added

---
+++ server/tests/server.test.js
@@ -0,0 +1,25 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = "teste todo";
+
+    request(app)
+      .post('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find()
+      })
+  })
+})
