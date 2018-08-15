319d090a03b8133d7166612c643deb73d3bb175a
danillo.moraes <danillo.moraes@outlook.com>
2018-08-15 09:24:41 -0300

    setup test and development env

--- server/server.js
+++ server/server.js
@@ -1,13 +1,4 @@
-var env = process.env.NODE_ENV || 'development';
-env = env.trim();
-
-if(env==='development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env==='test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
+require('./config/config.js')
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -124,5 +115,5 @@ app.patch('/todos/:id', (req, res) => {
 module.exports={app}
 
 app.listen(process.env.PORT, () => {
-  console.log(`Started server on port ${port}`);
+  console.log(`Started server on port ${process.env.PORT}`);
 });
